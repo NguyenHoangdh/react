2ffa816d6adec3cae6dc2edd9c8a867dcc792339
NguyenHoang <hoangpmhuevn@gmail.com>
2026-09-19 10:10:43 +0700

    tách component

--- 8.reactjs-tuhoc.cc-main/14.0 props-start/src/App.jsx
+++ 8.reactjs-tuhoc.cc-main/14.0 props-start/src/App.jsx
@@ -1,51 +1,9 @@
-import PropTypes from "prop-types";
-
-import logo from "./assets/logo-tuhoc.png";
-import pic1 from "./assets/pic1.png"
-import pic2 from "./assets/pic2.png"
-import pic3 from "./assets/pic3.png"
-import pic4 from "./assets/pic4.png"
-
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
 import { myData } from "../data";
 
-const obj = {
-  name: "Nam",
-  age: "28"
-};
 
-function Header() {
-  return (
-    <>
-      <header>
-        <img src={logo} alt="Tự Học" />
-        <h1>React Tuhoc.cc</h1>
-        <p>
-          Học React - Khám phá cách xây dựng ứng dụng linh hoạt, hiện đại, và
-          đầy sáng tạo!
-        </p>
-      </header>
-    </>
-  );
-}
-function MainContent({image, title, desc}) {
-  return (
-  <li>
-    <img src={image} alt={title} />
-    <h2>{title}</h2>
-    <p>{desc}</p>
-  </li>
-  )
-}
-MainContent.propTypes = {
-  image : PropTypes.string.isRequired,
-  title : PropTypes.string.isRequired,
-  desc : PropTypes.string
-}
 function App() {
-  console.log(myData[0].title);
-  console.log(myData[0].image);
-  console.log(myData[0].desc);
-
   return (
     <>
       <Header />
