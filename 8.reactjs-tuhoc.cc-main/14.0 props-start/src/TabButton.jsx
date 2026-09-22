66e744d80b2ae5518a261851fd3013c1e38e807a
NguyenHoang <hoangpmhuevn@gmail.com>
2026-09-22 10:47:55 +0700

    onClick beginning

--- 8.reactjs-tuhoc.cc-main/14.0 props-start/src/TabButton.jsx
+++ 8.reactjs-tuhoc.cc-main/14.0 props-start/src/TabButton.jsx
@@ -1,10 +1,21 @@
 export default function TabButton({children}) {
+    //js
+    // const btn = document.querySelector("button");
+    // btn.addEventListener("click", ()=>{//tham số 1 là lắng nghe sk, tham số 2 là thực hiện hàm
+    //     //làm việc gì đó
+    // }) 
+
+    function handleClick() {
+        alert("Nút bấm được click");
+    }
+
     return (
         <li>
-            <button>
+            <button onClick={handleClick}>
                 {children}
             </button>
         </li>
+        // ko thêm dấu ngoặc tròn vì thêm nó thì hàm thực thi ngay lập tức
     )
 }
 
